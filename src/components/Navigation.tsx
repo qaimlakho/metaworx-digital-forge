78d0820f3c9b7d86580ce4613c3a630011fd239d
hashirSoomro <hashirsomroo@gmail.com>
2025-08-14 23:14:18 +0500

    Fixed Bugs and Navbar size reduced

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -61,7 +61,7 @@ const Navigation = () => {
       isScrolled ? 'bg-white/95 backdrop-blur-md shadow-lg' : 'bg-transparent'
     }`}>
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex justify-between items-center h-24">
+        <div className="flex justify-between items-center h-20">
           {/* Logo */}
           <div className="flex-shrink-0">
             <Link 
